d9c321df0f08b96c8c3faf864c6956e8439effed
Desmond Harris <desmondharris912@gmail.com>
2022-09-10 02:10:58 -0500

    Now added chat server I guess
    The first time didn't work so trying again

--- chatclient.py
+++ chatclient.py
@@ -2,8 +2,8 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-# HOST = "192.168.45.10"
-HOST = "127.0.0.1"
+HOST = "192.168.45.10"
+#HOST = "127.0.0.1"
 PORT = 6969
 def connection(user):
     server.connect((HOST, PORT))
