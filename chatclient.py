e3d9cd34ce570ad819fdba68cbf82bb395eedf9c
Desmond Harris <desmondharris912@gmail.com>
2022-09-10 02:08:41 -0500

    added chatserver

--- chatclient.py
+++ chatclient.py
@@ -2,7 +2,8 @@ import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-HOST = "192.168.45.10"
+# HOST = "192.168.45.10"
+HOST = "127.0.0.1"
 PORT = 6969
 def connection(user):
     server.connect((HOST, PORT))
@@ -12,7 +13,7 @@ def connection(user):
 
 class socketHandling():
     def sendMessage(message):
-        a = server.send(bytearray(message + '\n', 'utf-8'))
+        a = server.send(bytearray(message.replace('\x00', '') + '\n', 'utf-8'))
         return
 
     def recMessage():
