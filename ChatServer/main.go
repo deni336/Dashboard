a760dc267c258ea3bcb286a62e57db5ccb864507
Desmond Harris <desmondharris912@gmail.com>
2023-01-13 18:35:45 -0600

    got server broadcast online and running

--- ChatServer/main.go
+++ ChatServer/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"chat/internal/screenshare"
 	"chat/internal/storage"
-	"chat/pkg/api"
 	"chat/pkg/clienthandler"
-	"chat/pkg/utils"
 	"fmt"
-	"log"
 )
 
 /*
@@ -32,7 +29,6 @@ var (
 	CHAT       = "localhost:6969"
 	SCRNSHR    = "localhost:7070"
 	FILEUPLOAD = "localhost:7777"
-	APIIP      = "localhost:1337"
 )
 
 var atv_usrs = clienthandler.InitializeActiveUserList()
@@ -52,41 +48,13 @@ func startSupportingServers(au *clienthandler.ActiveUsers) {
 
 	fmt.Println("Initializing file upload server...")
 	go storage.HostUploadServer(FILEUPLOAD)
-
-	fmt.Println("Initializing chat server API...")
-	api.API(APIIP, au)
 }
 
 func startChatServer(atv_usrs *clienthandler.ActiveUsers) {
 	fmt.Println("Initializing chat server...")
 
-	srv, err := clienthandler.Run(CHAT)
+	err := clienthandler.Run(CHAT)
 	if err != nil {
 		fmt.Println("failed setting up chat server")
 	}
-
-	for {
-		conn, err := clientListener.Accept()
-		if err != nil {
-			log.Print(err)
-			fmt.Println("Failed to start chat server")
-			continue
-		}
-
-		user := &clienthandler.User{
-			Name:        utils.ReadBuf(conn),
-			Address:     conn.RemoteAddr().String(),
-			Connection:  conn,
-			IsConnected: true,
-		}
-
-		atv_usrs.Add(user.Name, user)
-		fmt.Println("User created and added to active_user list")
-		fmt.Println(atv_usrs.ListUsers())
-		go clienthandler.HandleConnection(user)
-	}
-}
-
-func ACTIVE_USRS() *clienthandler.ActiveUsers {
-	return atv_usrs
 }
