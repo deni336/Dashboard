b9e7adf3e906b1ffcbaf3243758cca43c3a8e084
deni336 <denison_carson@hotmail.com>
2023-01-19 00:30:26 -0600

    Dashboard

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 from grpc import *
+import protos.kasugai_pb2 as chat
+import protos.kasugai_pb2_grpc as rpc
 
+taskQue = []
+
+stub = rpc.BroadcastStub
 
 async def chatSub():
     async with Channel("localhost", "6969") as channel:
         pass
+
+    async with stub.ChatService.open() as stream:
+            await stream.ChatService()
+
+def que(self, inp):
+    taskQue.append(inp)
